b496dd593e3404894f497370bd95ea667e4e8d51
danilobrm <danilobr.web@gmail.com>
2023-02-18 00:53:49 -0300

    App

--- pokedex-web/src/App.js
+++ pokedex-web/src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyle from './styles/GlobalStyle';
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
